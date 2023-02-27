727384d07fe5c3fb9d18071533c331f0d6d83d2f
mybil <mybil@NICOLE>
2023-02-26 23:38:50 -0500

    Add project files.

---
+++ AbstractAnimalClass/Program.cs
@@ -0,0 +1,53 @@
+﻿abstract class Animal
+{
+    // Property
+    public abstract string Name { get; set; }
+    // Methods
+    public abstract string Describe();
+    public string whatAmI()
+    {
+        return "I am an animal";
+    }
+}
+class Program
+{
+    class Dog : Animal
+    {
+        public override string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }   
+
+        public Dog()
+        {
+            Name = string.Empty;
+            Breed = string.Empty;
+        }
+        public Dog(string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+        
+        public override string Describe()
+        {
+            return "I am a dog." + "\nMy name is " + Name + "." + "\nMy breed is " + Breed + "." + "\nI am the color " + Color + ".";
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        
+        Dog scooby = new Dog();
+        scooby.Name = "Scooby";
+        scooby.Breed = "Great Dane";
+        scooby.Color = "Brown";
+        Console.WriteLine(scooby.whatAmI());
+        Console.WriteLine(scooby.Describe());
+
+        Dog snoopy = new Dog("Snoopy", "Beagle", "White");
+        Console.WriteLine();
+        Console.WriteLine(snoopy.whatAmI());
+        Console.WriteLine(snoopy.Describe());
+    }
+}
